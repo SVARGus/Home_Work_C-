1a73b4df3197606059776f8278610b546ef01647
SVARGus <kyznetsovpn@mail.ru>
2024-11-12 23:23:22 +0300

    ДЗ по сериализации

---
+++ HomeWork_11_11_2024/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+/*
+Домашнее задание по теме сериализация
+*/
+public class Program
+{
+    public static void Main()
+    {
+
+    }
+}
+
+[Serializable]
+class menu
+{
+    public string id { get; set; }
+    public string value { get; set; }
+
+}
